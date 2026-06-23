a2a39dcfc8e1eb382c603a3aae41e213dafbbd6f
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2026-06-23 12:05:56 +0530

    added new project images

--- src/app/components/projects/projectImages.ts
+++ src/app/components/projects/projectImages.ts
@@ -2,13 +2,13 @@ import { StaticImageData } from "next/image"
 import Blogito from "@public/Blogito.png"
 import OnTrack from "@public/onTrack.webp"
 import SecureVault from "@public/securevault.jpg"
-import TestimonialsLo from "@public/testimonialslo.png"
+import TestimonialsLo from "@public/testimonialslo.webp"
 import FitKaka from "@public/fitkaka.webp"
 import Syncoo from "@public/syncoo.webp"
 import CanvasBoard from "@public/canvas.webp"
 
 export const projectImages: Record<string, StaticImageData> = {
-  "testimonialslo.png": TestimonialsLo,
+  "testimonialslo.webp": TestimonialsLo,
   "Blogito.png": Blogito,
   "onTrack.webp": OnTrack,
   "securevault.jpg": SecureVault,
